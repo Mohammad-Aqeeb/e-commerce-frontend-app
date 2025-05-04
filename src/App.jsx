ad93d7b7b78397faecafdf98ae4ae26f7cbc64bd
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-05-04 19:44:55 +0530

    change in app

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,25 @@ import HomePage from './pages/HomePage';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import { useState } from 'react';
+import SingleProduct from './components/SingleProduct';
 
 
 function App() {
 
-  const [user, setUser] =useState(undefined);
+  const [user, setUser] = useState(undefined);
+  const [products, setProducts] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState({});
 
+  console.log(selectedProduct)
   return (
     <div className='contaier'>
-
       <Routes>
         <Route path='/signup' element = {<Signup setUser={setUser}/>}></Route>
         <Route path='/login' element = {<Login setUser={setUser}/>}></Route>
 
-        <Route path='/' element = {<HomePage user={user}/>}></Route>
-        <Route path='/cart' element = {<CartPage/>}></Route>
+        <Route path='/' element = {<HomePage user={user} products={products} setProducts={setProducts} setSelectedProduct={setSelectedProduct}/>}></Route>
+        <Route path='/cart' element = {<CartPage user={user}/>}></Route>
+        <Route path='/singlePage' element = {<SingleProduct seletedProduct={selectedProduct}/>}></Route>
       </Routes>
     </div>
   )
