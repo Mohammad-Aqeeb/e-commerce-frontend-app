b5550dced481f6811ae603794a23027f036f0e08
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-05-05 22:43:48 +0530

    change in products component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,13 +1,29 @@
 import { useNavigate } from "react-router-dom";
 import "./Product.css";
+import axios from "axios";
 
-function Product({ product, setSelectedProduct}) {
+function Product({user, product, setSelectedProductId}) {
 
     const navigate = useNavigate();
 
     function singleProductHandler(){
-        setSelectedProduct(product);
-        navigate("/singlePage");
+        setSelectedProductId(product._id);
+        navigate(`/singlePage/${product._id}`);
+    }
+
+    async function addToCartHandler(){
+        try{
+            console.log(user);
+            console.log(user._id);
+            console.log(product._id);
+            
+            const data = await axios.post("http://localhost:4000/api/v1/addProductToCart",{UserId : user._id, productId : product._id});
+            console.log("AAAAAAaaaaaa");
+            console.log(data);
+        }
+        catch(error){
+            console.log(error);
+        }
     }
 
     let description = `${product.description.substring(0,200)}...`
@@ -25,7 +41,7 @@ function Product({ product, setSelectedProduct}) {
                     <span className="productCategory">{product.category.categoryName}</span>
                 </div>
 
-                <button className="AddToCartButton">Add To Cart</button>
+                <button className="AddToCartButton" onClick={addToCartHandler}>Add To Cart</button>
             </div>
         </div>
     );
